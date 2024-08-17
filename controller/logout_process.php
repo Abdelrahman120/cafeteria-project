4d79ecbc6f9f527275b332db161292b041993893
“hamo4651” <hamo4651@gmail.com>
2024-08-17 22:38:22 +0300

    product_user

--- controller/logout_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-if ($_SESSION['login']) {
-    $_SESSION = [];
-    session_destroy();
-    header("Location: ../login_form.php");
-}
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-header("Location: login_form.php"); // Redirect to the login page
-exit();
-?>
